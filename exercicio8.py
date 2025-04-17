b8f1a9d1cedbe8d7a258de3e1a58c23293d313da
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-17 08:00:47 -0300

    Teste

--- exercicio8.py
+++ exercicio8.py
@@ -3,15 +3,15 @@ serie_harmonica = []
 serie = 0
 soma = 0
 
-if num <= 0:
+if num < 0:
     print("INVALIDO")
+elif num == 0:
+    print("OK")
 else:
-    for i in range(1, num+1):
+    for i in range(1, num + 1):
         serie = f"1/{i}"
-        soma += 1/i
+        soma += 1 / i
         serie_harmonica.append(serie)
-
-
 
     print(serie_harmonica)
     print(f'A soma é: {soma:.2f}')
