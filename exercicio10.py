bd3ff95456746b403e7aaaea07c321aa087ff192
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-16 23:54:25 -0300

    Correção EX 10

--- exercicio10.py
+++ exercicio10.py
@@ -1,43 +1,30 @@
 import math
 
-inicio = int(input("Digite o inicio da lista: "))
-fim = int(input("Digite o final da lista: "))
+try:
+    inicio = int(input("Digite o inicio da lista: "))
+    fim = int(input("Digite o final da lista: "))
 
-if inicio <= 0 or fim <= 0:
-    print("INVALIDO")
-else:
-    if inicio > fim:
+    if inicio <= 0 or fim <= 0:
         print("INVÁLIDO")
-        print("O segundo número precisa ser maior que o segundo")
+    elif inicio > fim:
+        print("INVÁLIDO")
+        print("O segundo número precisa ser maior que o primeiro")
     else:
         kaprekar = []
-        quadrado = 0
-        str_esquerda = ''
-        str_direita = ''
-        num_esquerda = 0
-        num_direita = 0
 
         for i in range(inicio, fim + 1):
-            quadrado = i ** 2
-            quadrado = str(quadrado)
+            quadrado = str(i ** 2)
+            meio = math.floor(len(quadrado) / 2)
 
-            meio = len(quadrado) / 2
-            meio = math.floor(meio)
+            str_direita = quadrado[meio:]
+            str_esquerda = quadrado[:meio]
 
-            str_direita = quadrado[meio : len(quadrado)]
-            str_esquerda = quadrado[0 : meio]
+            num_direita = int(str_direita) if str_direita else 0
+            num_esquerda = int(str_esquerda) if str_esquerda else 0
 
-            if str_direita != '':
-                num_direita = int(str_direita)
-            if str_esquerda != '':
-                num_esquerda = int(str_esquerda)
-
-            soma = num_esquerda + num_direita
-
-            if soma == i:
+            if num_direita + num_esquerda == i:
                 kaprekar.append(i)
 
-        if inicio < 1:
-            kaprekar.remove(0)
-
         print(f'O números de kaprekar de {inicio} até {fim} são: {kaprekar}')
+except EOFError:
+    print("ERRO")
