8f5a529d68122fcfdb967471035e408b9dce3c3d
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-16 23:45:54 -0300

    Correção Exercicio 08

--- exercicio8.py
+++ exercicio8.py
@@ -2,12 +2,16 @@ num = int(input("Digite um número: "))
 lista = []
 serie = 0
 soma = 0
-for i in range(1, num+1):
-    serie = f"1/{i}"
-    soma += 1/i
-    lista.append(serie)
 
-serie_formatada = " + ".join(lista)
+if num == 0:
+    print("INVALIDO")
+else:
+    for i in range(1, num+1):
+        serie = f"1/{i}"
+        soma += 1/i
+        lista.append(serie)
 
-print(f'A série harmônica de {num} é {serie_formatada}')
-print(f'A soma é: {soma:.2f}')
+    serie_formatada = " + ".join(lista)
+
+    print(f'A série harmônica de {num} é {serie_formatada}')
+    print(f'A soma é: {soma:.2f}')
