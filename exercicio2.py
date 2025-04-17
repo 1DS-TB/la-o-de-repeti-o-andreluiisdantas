85327c3f04828f715e0ff77b605fff6b048650d0
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-16 23:26:51 -0300

    Correção Exercicio 02

--- exercicio2.py
+++ exercicio2.py
@@ -1,6 +1,10 @@
 num = int(input("Digite um número"))
 soma = 0
-while num > 0:
-    soma = soma + num
-    num = num - 1
-print(soma)
+
+if num < 0:
+    print("INVALIDO")
+else:
+    while num > 0:
+        soma = soma + num
+        num = num - 1
+    print(soma)
