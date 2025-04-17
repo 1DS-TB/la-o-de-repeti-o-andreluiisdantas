84b1e1511f6dcb3b0d33591da7880600f6daeb26
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-17 08:17:47 -0300

    Teste

--- exercicio3.py
+++ exercicio3.py
@@ -1,9 +1,6 @@
 N = int(input("Digite um número "))
 
-if N <= 0:
-    print("INVALIDO")
-else:
-    for i in range(1, 11):
-        resultado = N * i
 
-        print(f'{i} * {N} = {resultado}')
+for i in range(1, 11):
+    resultado = N * i
+    print(f'{i} * {N} = {resultado}')
