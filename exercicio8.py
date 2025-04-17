9bec2fbefd3b9824b0935904c7e6db49f221824b
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-17 00:09:38 -0300

    Exercicio 08

--- exercicio8.py
+++ exercicio8.py
@@ -3,7 +3,7 @@ lista = []
 serie = 0
 soma = 0
 
-if num < 0:
+if num <= 0:
     print("INVALIDO")
 else:
     for i in range(1, num+1):
@@ -11,9 +11,7 @@ else:
         soma += 1/i
         lista.append(serie)
 
-    if num == 0:
-        print("INVALIDO")
-    else:
-        serie_formatada = " + ".join(lista)
-        print(f'A série harmônica de {num} é {serie_formatada}')
-        print(f'A soma é: {soma:.2f}')
+    serie_formatada = " + ".join(lista)
+
+    print(f'A série harmônica de {num} é {serie_formatada}')
+    print(f'A soma é: {soma:.2f}')
