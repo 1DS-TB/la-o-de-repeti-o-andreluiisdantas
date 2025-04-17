2379028fa9f2b6fd16cb6ec4dc8ddeb28263ab92
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-17 08:10:27 -0300

    Correção EX 5

--- exercicio5.py
+++ exercicio5.py
@@ -1,17 +1,16 @@
 num = int(input("Digite um número: "))
-primo = True
+
 if num == 0 or num == 1:
-    print(f'O número {num} não é primo')
-elif num > 0:
+    print("OK")
+elif num < 0:
+    print("INVALIDO")
+else:
+    primo = True
     for i in range(2, num):
         if num % i == 0:
             primo = False
             break
-    if primo == True:
+    if primo:
         print(f'O número {num} é primo')
     else:
         print(f'O número {num} não é primo')
-else:
-    print("INVALIDO")
-
-
