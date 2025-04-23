324074f41adce4f134fabbb72a91ee8708d63887
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-23 07:48:43 -0300

    Correção Exercicio 3

--- exercicio3.py
+++ exercicio3.py
@@ -3,7 +3,9 @@ num = int(input("Digite um número "))
 if num < 0:
     print("INVALIDO")
 else:
-    for i in range(1, 11):
-        resultado = num * i
-
-        print(f'{i} * {num} = {resultado}')
+    if num == 0:
+        print("Tabuada do 0 não existe.")
+    else:
+        for i in range(1, 11):
+            resultado = num * i
+            print(f'{i} * {num} = {resultado}')
