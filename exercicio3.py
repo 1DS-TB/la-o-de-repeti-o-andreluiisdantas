0687591bb32b5a6f8c45c861bc1ea58165f9a485
Andre Luis Sousa Dantas <52958920852@>
2025-04-23 15:39:17 -0300

    Correção Exercicio 3

--- exercicio3.py
+++ exercicio3.py
@@ -1,9 +1,7 @@
 num = int(input("Digite um número "))
 
-if num < 0:
-    print("INVALIDO")
-else:
-    for i in range(1, 11):
-        resultado = num * i
 
-        print(f'{i} * {num} = {resultado}')
+for i in range(1, 11):
+    resultado = num * i
+
+    print(f'{num} x {i} = {resultado}')
