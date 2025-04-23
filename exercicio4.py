74c128f9e1430074dc61c0d224ddf3051a57dbf7
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-23 07:57:28 -0300

    Teste

--- exercicio4.py
+++ exercicio4.py
@@ -1,12 +1,12 @@
-num = int(input("Digite um número para calcular fatoria: "))
+N = int(input("Digite um número para calcular fatoria: "))
 
 fatorial = 1
 
-if num == 0:
+if N == 0:
     print(f'A fotorial é {fatorial}')
-elif num < 0:
+elif N < 0:
     print("INVALIDO")
 else:
-    for i in range(1, num + 1):
+    for i in range(1, N + 1):
         fatorial *= i
     print(f'A fotorial é {fatorial}')
