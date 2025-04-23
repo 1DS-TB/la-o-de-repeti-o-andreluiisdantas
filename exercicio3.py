f746786a3378c53fe009b9efcd1f0408439ce24b
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-23 07:53:31 -0300

    Teste Ex3

--- exercicio3.py
+++ exercicio3.py
@@ -1,11 +1,5 @@
 num = int(input("Digite um número "))
 
-if num < 0:
-    print("INVALIDO")
-else:
-    if num == 0:
-        print("Tabuada do 0 não existe.")
-    else:
-        for i in range(1, 11):
-            resultado = num * i
-            print(f'{i} * {num} = {resultado}')
+for i in range(1, 11):
+    resultado = num * i
+    print(f'{i} X {num} = {resultado}')
