4afeefab97147b5843a3ccbe8533c974fcca98b6
Andre Luis Sousa Dantas <52958920852@>
2025-04-23 16:23:02 -0300

    Correção Exercicio 10

--- exercicio10.py
+++ exercicio10.py
@@ -1,27 +1,24 @@
 import math
 
-try:
-    inicio = int(input("Digite o inicio da lista: "))
-    fim = int(input("Digite o final da lista: "))
+inicio = int(input("Digite o inicio da lista: "))
+fim = int(input("Digite o final da lista: "))
 
-    if inicio > fim:
-        print("INVALIDO")
-    else:
-        kaprekar = []
+if inicio > fim:
+    print("INVALIDO")
+else:
+    kaprekar = []
+    for i in range(inicio, fim + 1):
+        quadrado = str(i ** 2)
+        meio = math.floor(len(quadrado) / 2)
 
-        for i in range(inicio, fim + 1):
-            quadrado = str(i ** 2)
-            meio = math.floor(len(quadrado) / 2)
+        str_direita = quadrado[meio:]
+        str_esquerda = quadrado[:meio]
 
-            str_direita = quadrado[meio:]
-            str_esquerda = quadrado[:meio]
+        num_direita = int(str_direita) if str_direita else 0
+        num_esquerda = int(str_esquerda) if str_esquerda else 0
 
-            num_direita = int(str_direita) if str_direita else 0
-            num_esquerda = int(str_esquerda) if str_esquerda else 0
+        if num_direita + num_esquerda == i:
+            kaprekar.append(i)
 
-            if num_direita + num_esquerda == i:
-                kaprekar.append(i)
+    print(f'O números de kaprekar de {inicio} até {fim} são: {kaprekar}')
 
-        print(f'O números de kaprekar de {inicio} até {fim} são: {kaprekar}')
-except EOFError:
-    print("ERRO")
