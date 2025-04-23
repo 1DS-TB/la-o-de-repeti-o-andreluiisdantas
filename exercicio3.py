3c1caee035c98de116cc1b4a0b2d9c7ce48baf11
andreluiisdantas <andreluissousaa10@gmail.com>
2025-04-23 07:56:12 -0300

    Teste 03

--- exercicio3.py
+++ exercicio3.py
@@ -1,5 +1,5 @@
-num = int(input("Digite um número "))
+N = int(input("Digite um número "))
 
 for i in range(1, 11):
-    resultado = num * i
-    print(f'{i} X {num} = {resultado}')
+    resultado = N * i
+    print(f'{i} X {N} = {resultado}')
